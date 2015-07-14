fb5c94b98637bd18314fc3578bc1f11679d79282
Johannes Dröge <johannes.droege@uni-duesseldorf.de>
2015-07-14 18:47:52 +0200

    add conversion script for coverage plotting in R

---
+++ coverage2covcol.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+u"""
+Read coverage file format and ouput samples as columns and positions as rows. There will be one file per input row and
+the output files are named after the sequence identifier.
+"""
+
+__author__ = "johannes.droege@uni-duesseldorf.de"
+
+import sys
+from itertools import repeat
+
+def unpack(e):
+    tmp = e.split(":", 2)
+    return tmp[0], tmp[1].split(",")
+
+zeros = repeat("0")
+
+if __name__ == "__main__":
+    samples = sys.argv[1:]
+
+    for line in sys.stdin:
+        if line and line[0] == "#":
+            continue
+
+        ident, rest = line.rstrip().split("\t", 2)
+
+        with open("%s.covcol" % ident, "w") as f:
+            data = dict((unpack(e) for e in rest.split(" ")))
+            for rv in zip(*[data.get(s, zeros) for s in samples]):
+                f.write("\t".join(rv))
+                f.write("\n")
