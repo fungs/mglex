d69472705995c7842bfceedb85bd2269dff8eab2
Johannes Dröge <johannes.droege@uni-duesseldorf.de>
2015-08-21 10:47:07 +0200

    stupid simple helper

--- labelmatrix-generate.py
+++ labelmatrix-generate.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-# takes the numer of data points and the number of the class as input and
+# takes the number of data points and the number of the class as input and
 # generates a matrix with according log-transformed 0/1 entries
 
 from sys import argv, stdout
 
 classindex = int(argv[1])
 classnum = int(argv[2])
-datapoints = int(argv[3])
+
+try:
+    datapoints = int(argv[3])
+except IndexError:
+    datapoints = 1
 
 line = "\t".join(["0.0" if i == classindex else "inf" for i in range(1, classnum+1)]) + "\n"
 
