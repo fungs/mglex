1d7259b2d476ce5c6fc30da224a8ddec6831718c
Johannes Dröge <johannes.droege@uni-duesseldorf.de>
2016-08-02 18:19:03 +0200

    new bincompare command in cli

---
+++ mglex/cli/bincompare.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+u"""
+DESCRIPTION
+
+Usage:
+  bincompare  (--help | --version)
+  bincompare  (--weight <file>) [--likelihood <file>] [--beta <float>]
+
+  -h, --help                        Show this screen
+  -v, --version                     Show version
+  -l <file>, --likelihood <file>    Likelihood matrix; default standard input
+  -w <file>, --weight <file>        Weights (sequence length) file
+  -b <float>, --beta <float>        Beta correction factor (e.g. determined via MSE evaluation); default 1.0
+"""
+
+import sys
+import numpy as np
+
+# some ugly code which makes this run as a standalone script
+try:  # when run inside module
+    from .. import *
+except SystemError:  # when run independenly, needs mglex package in path
+    try:
+        from mglex import *
+    except ImportError:
+        from pathlib import Path
+        sys.path.append(str(Path(__file__).resolve().parents[2]))
+        from mglex import *
+
+__author__ = "johannes.droege@uni-duesseldorf.de"
+from mglex import __version__
+
+
+def main(argv):
+    from docopt import docopt
+    argument = docopt(__doc__, argv=argv, version=__version__)
+    common.handle_broken_pipe()
+
+    # load input
+    if argument["--likelihood"]:
+        likelihood = common.load_probmatrix_file(argument["--likelihood"])
+    else:
+        likelihood = common.load_probmatrix(sys.stdin)
+
+    if argument["--beta"]:
+        likelihood *= types.logprob_type(argument["--beta"])
+
+    weights = common.load_seqlens_file(argument["--weight"])
+    distmat = evaluation.similarity_matrix(likelihood, weights)
+    common.write_probmatrix(distmat)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
