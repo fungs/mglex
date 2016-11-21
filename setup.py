7d63360c24008bb4b87eb21211543eee89736060
Johannes Dröge <code@fungs.de>
2016-11-21 17:52:25 +0100

    improve python source package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='mglex - MetaGenome Likelihood Extractor',
     url='http://github.com/fungs/mglex',
     author='Johannes Dröge',
-    author_email='johannes.droege@uni-duesseldorf.de',
+    author_email='code@fungs.de',
     license='GNU General Public License, version 3 (GPL-3.0)',
     packages=find_packages(),
     exclude_package_data = {'': ['.gitignore']},
@@ -15,7 +15,6 @@ setup(
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     install_requires=["numpy >= 1.9.2", "scipy >= 0.16.0", "docopt >= 0.6.2"],
-    # long_description=read_file('README.md'),
     classifiers=[
         "Development Status :: 3 - Beta",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
