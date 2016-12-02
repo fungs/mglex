1496307c7732a832bd1c0ea27951cb69466269bd
Johannes Dröge <code@fungs.de>
2016-12-02 14:47:06 +0100

    adjust distribution package name and metadata

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
+from os import path
+
+
+def readme():
+    here = path.relpath(path.abspath(path.dirname(__file__)))    
+    with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+            return f.read()
+
 
 setup(
-    name='mglex',
-    description='mglex - MetaGenome Likelihood Extractor',
-    url='http://github.com/fungs/mglex',
+    name='MGLEX',
+    description='MGLEX - MetaGenome Likelihood EXtractor',
+    long_description=readme(),
+    url='https://github.com/fungs/mglex',
     author='Johannes Dröge',
     author_email='code@fungs.de',
     license='GNU General Public License, version 3 (GPL-3.0)',
@@ -19,5 +28,11 @@ setup(
         "Development Status :: 3 - Beta",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Operating System :: POSIX :: Linux"
     ]
 )
